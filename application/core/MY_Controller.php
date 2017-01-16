221ce6ecd89fbbf332f3d63a879c46bd4136f26c
hemuhan <hemuhan@qq.com>
2017-01-16 23:12:41 +0800

    添加未提交的东西

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hemuhan
+ * Date: 17/1/16
+ * Time: 下午4:15
+ * @property array $userinfo 用户信息
+ */
+
+class MY_Controller extends CI_Controller {
+    protected $userinfo;
+
+
+}
